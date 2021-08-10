64c6d8520c5d30c018477b159fc80c656f41b156
scarletYewon <kwn09032@gmail.com>
2021-08-11 01:28:23 +0900

    Create 하노이탑10.py

---
+++ 과소사/하노이탑10.py
@@ -0,0 +1,12 @@
+def hanoi(n, start, mid, end):
+    if n == 1:
+        print(start, end)
+    else:
+        hanoi(n - 1, start, end, mid)
+        print(start, end)
+        hanoi(n - 1, mid, start, end)
+
+
+n = int(input())
+print(2 ** n - 1)
+hanoi(n, 1, 2, 3)
